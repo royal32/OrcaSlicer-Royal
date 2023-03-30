cdf9ba1f740e6785e24e36e98c51d222856fefcb
Seth Phillips <sethphillips97@gmail.com>
2023-03-29 22:52:52 -0400

    Add fast iter. compilation option to win build

--- build_release.bat
+++ build_release.bat
@@ -6,6 +6,9 @@ set DEPS=%CD%/OrcaSlicer_dep
 if "%1"=="studio" (
     GOTO :studio
 )
+if "%1"=="fast" (
+    GOTO :studio
+)
 echo "building deps.."
 cmake ../ -G "Visual Studio 16 2019" -DDESTDIR="%CD%/OrcaSlicer_dep" -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release --target ALL_BUILD -- -m
@@ -18,6 +21,12 @@ cd %WP%
 mkdir build 
 cd build
 
+if "%1"=="fast" (
+    GOTO :fastjump
+)
+
 cmake .. -G "Visual Studio 16 2019" -DBBL_RELEASE_TO_PUBLIC=1 -DCMAKE_PREFIX_PATH="%DEPS%/usr/local" -DCMAKE_INSTALL_PREFIX="./OrcaSlicer" -DCMAKE_BUILD_TYPE=Release -DWIN10SDK_PATH="C:/Program Files (x86)/Windows Kits/10/Include/10.0.19041.0"
 cmake --build . --config Release --target ALL_BUILD -- -m
+:fastjump
 cmake --build . --target install --config Release
+
